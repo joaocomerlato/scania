9d1489e58c70c375f3f2c45fcb4a208c21882767
joaocomerlato <164171670+joaocomerlato@users.noreply.github.com>
2025-05-26 08:08:23 -0300

    Add files via upload

--- csript.js
+++ csript.js
@@ -2,7 +2,18 @@ document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    botaoDeAcessibilidade.addEventListener('click', function (){
+     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+  })
+
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+   
+    const alternaContraste = document.getElementById('alterna-contraste')
+
     let tamanhoAtualFonte = 1;
+
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
@@ -15,7 +26,9 @@ document.addEventListener('DOMContentLoaded', function(){
 
     });
 
-});
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+   })
 
-const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+});
